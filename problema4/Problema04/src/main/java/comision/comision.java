6da3d9c1179f97d8b047d475ad0a65ac5d3760c4
Daniel Ojeda <dmojeda@utpl.edu.ec>
2023-05-06 21:41:37 -0500

    DIA

--- problema4/Problema04/src/main/java/comision/comision.java
+++ problema4/Problema04/src/main/java/comision/comision.java
@@ -9,23 +9,5 @@ package comision;
  * @author VEL-USER
  */
 public class comision {
-    private String nombreCliente;
-    private String nombreBanco;
-    private double valorCheque;
-    private double comisiónBanco;
-    
-    public comision(){
-        nombreCliente = "Mochix Orix";
-        nombreBanco = "moci Cia. Ltda.";
-        valorCheque = 2000;
-        comisiónBanco = 20;
-    }
-    
-    public comision(String n, String nB, double v, double c){
-        nombreCliente = n;
-        nombreBanco = nB;
-        valorCheque = v;
-        comisiónBanco = c;
-    }
     
 }
